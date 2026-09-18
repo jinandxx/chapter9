ccd0551cb9c69b37c6fe3f2f9cca890313793507
jinandxx <jinandxx@gmail.com>
2026-09-18 20:22:41 +0900

    base config revert

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/chapter9/' : '/',
-}))
+})
