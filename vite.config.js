7f6e59cc6f53f4e5f840ef7ba54be3bf3545dc4c
jinandxx <jinandxx@gmail.com>
2026-09-18 20:10:57 +0900

    base change 3

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base : command === 'build' ? '/chapter9/' :'/',
-})
+  base: command === 'build' ? '/chapter9/' : '/',
+}))
